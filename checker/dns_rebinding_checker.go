b77558ec8334e7ef3f24b5d6b64278fae78c1a8d
Thng Kai Yuan <thngkaiyuan@users.noreply.github.com>
2016-10-16 15:45:24 +0800

    Update DNS rebinding checker description

--- checker/dns_rebinding_checker.go
+++ checker/dns_rebinding_checker.go
@@ -3,6 +3,9 @@ package checker
 type DnsRebindingChecker struct{}
 
 func (c DnsRebindingChecker) Check(domain string, resultChannel chan CheckerResult) {
+	// If the domain does not support HTTP (i.e. only supports HTTPS), then it is secure
+	// Otherwise we check if we are able to connect to the domain using an invalid Host field in the header
+	
 	// Do whatever you wanna check, and put result into the channel.
 	// This is a stub result, you need to change it.
 	okUrls := []string{
@@ -14,10 +17,10 @@ func (c DnsRebindingChecker) Check(domain string, resultChannel chan CheckerResu
 		"youtube.com",
 	}
 	result := CheckerResult{
-		Title:            "Validation of \"Host\" field",
-		OkDescription:    "The following subdomains checks the \"Host\" field in the HTTP(S) request header.",
+		Title:            "In this scan, our server will attempt to connect to your domain(s) using an invalid *Host header*. If it is successful in doing so, then your domain might be used as a target of DNS rebinding attacks.\nWeb servers which reject HTTP requests with unrecognized Host headers or which strictly requires HTTPS connections are safe against DNS rebinding attacks.",
+		OkDescription:    "Safe either because the web server rejects unrecognized Host headers or strictly requires HTTPS",
 		OkUrls:           okUrls,
-		NotOkDescription: "The following subdomains ignores the \"Host\" field of the HTTP(S) request header. They might be vulnerable to *DNS Rebinding Attack*.",
+		NotOkDescription: "Unsafe because the invalid Host header was ignored and HTTP connections are supported",
 		NotOkUrls:        notOkUrls,
 	}
 	resultChannel <- result
