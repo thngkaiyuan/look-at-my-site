f6705869bd2c68ab348b9847d0672536f5f0f4f0
Xinan Liu <xinan@me.com>
2016-10-17 19:11:00 +0800

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,15 @@ PID_FILE="./server.pid"
 LOG_FILE="./server.log"
 ERR_FILE="./server.err"
 
+make deps
+make build
+
 if [ -f $PID_FILE ]; then
     PID=$(cat $PID_FILE)
     /bin/kill -9 $PID
     /bin/rm $PID_FILE
 fi
 
-make deps
-make build
 ./look-at-my-site > $LOG_FILE 2> $ERR_FILE &
 PID=$!
 echo $PID > $PID_FILE
